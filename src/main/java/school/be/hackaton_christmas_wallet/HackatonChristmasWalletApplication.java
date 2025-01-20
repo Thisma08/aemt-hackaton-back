48ffb058188202465bf6aad1a03ee3c6f7453b84
Vandijstadt Nicolas <81445233+vandijstadt@users.noreply.github.com>
2025-01-20 11:49:00 +0100

    ajouts du exclude

--- src/main/java/school/be/hackaton_christmas_wallet/HackatonChristmasWalletApplication.java
+++ src/main/java/school/be/hackaton_christmas_wallet/HackatonChristmasWalletApplication.java
@@ -2,10 +2,10 @@ package school.be.hackaton_christmas_wallet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class HackatonChristmasWalletApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(HackatonChristmasWalletApplication.class, args);
     }
