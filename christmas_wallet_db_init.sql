adaadfd28547676a9e196a90d4bf24347ec4822d
Mathis Mahaux <77389133+Thisma08@users.noreply.github.com>
2025-01-20 11:45:23 +0100

    Update christmas_wallet_db_init.sql

--- christmas_wallet_db_init.sql
+++ christmas_wallet_db_init.sql
@@ -8,45 +8,36 @@ create table users
 (
     id bigint auto_increment primary key,
     username varchar(255) not null,
-    email varchar(255) not null,
     password varchar(255) not null,
-    constraint UK_email unique (email),
     constraint UK_username unique (username)
+);
+
+create table categories
+(
+    id bigint auto_increment primary key,
+    name varchar(255) not null
 );
 
 create table purchases
 (
     id bigint auto_increment primary key,
-    userId bigint not null,
     amount decimal(10,2) not null,
-    purchaseDate datetime not null
+    userId bigint not null,
+    categoryId bigint not null,
+    purchaseDate datetime not null,
+    constraint FK_userId foreign key (userId) references users (id),
+    constraint FK_categoryId foreign key (categoryId) references categories (id)
 );
-
-create table months
-(
-    id bigint auto_increment primary key,
-    month varchar(9) not null
-);
-
-insert into months(month)
-values
-('January'),
-('February'),
-('March'),
-('April'),
-('May'),
-('June'),
-('July'),
-('August'),
-('September'),
-('October'),
-('November'),
-('December');
 
 create table budgets
 (
     id bigint auto_increment primary key,
     budget decimal(10,2)not null,
-    monthId bigint not null,
-    constraint FK_monthId_id foreign key (id) references months (id)
+    month bigint not null,
+    year bigint not null
 );
+
+create index idx_purchase_user on purchases (userId);
+create index idx_purchase_category on purchases (categoryId);
+create index idx_purchase_date on purchases (purchaseDate);
+create index idx_budget_month on budgets (monthNumber);
