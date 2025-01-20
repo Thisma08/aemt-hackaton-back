e33b774dfc90c8f87c18860774fb7a90dec45f7b
Vandijstadt Nicolas <81445233+vandijstadt@users.noreply.github.com>
2025-01-20 14:10:05 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/school/be/hackaton_christmas_wallet/controller/ControllerBudget.java

--- src/main/java/school/be/hackaton_christmas_wallet/controller/ControllerBudget.java
+++ src/main/java/school/be/hackaton_christmas_wallet/controller/ControllerBudget.java
@@ -1,10 +1,12 @@
 package school.be.hackaton_christmas_wallet.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import school.be.hackaton_christmas_wallet.application.Budget.BudgetQueryProcessor;
+import school.be.hackaton_christmas_wallet.application.Budget.command.BudgetCommandProcessor;
+import school.be.hackaton_christmas_wallet.application.Budget.command.CreateBudget.CreateBudgetCommand;
+import school.be.hackaton_christmas_wallet.application.Budget.command.CreateBudget.CreateBudgetOutput;
+import school.be.hackaton_christmas_wallet.application.Budget.query.GetBudget.GetAllBudgetOutput;
 
 @RestController
 @RequestMapping("/v1/Budget")
