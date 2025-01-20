cb64ef128fcef1d0d977fdb57218a1313c1f96e5
Vandijstadt Nicolas <81445233+vandijstadt@users.noreply.github.com>
2025-01-20 09:38:05 +0100

    Inititial commit

---
+++ src/main/java/school/be/hackaton_christmas_wallet/HackatonChristmasWalletApplication.java
@@ -0,0 +1,13 @@
+package school.be.hackaton_christmas_wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackatonChristmasWalletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackatonChristmasWalletApplication.class, args);
+    }
+
+}
---
+++ src/test/java/school/be/hackaton_christmas_wallet/HackatonChristmasWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package school.be.hackaton_christmas_wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackatonChristmasWalletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
