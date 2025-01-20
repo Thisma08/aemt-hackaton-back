57e7ddb1bd39cc73998f9089ea4a450bbfcfa4ed
Vandijstadt Nicolas <81445233+vandijstadt@users.noreply.github.com>
2025-01-20 12:01:45 +0100

    Creation de la version test du controller

--- src/main/java/school/be/hackaton_christmas_wallet/controller/ControllerBudget.java
+++ src/main/java/school/be/hackaton_christmas_wallet/controller/ControllerBudget.java
@@ -1,17 +1,16 @@
 package school.be.hackaton_christmas_wallet.controller;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/admin")
-public class ControllerTest {
+@RequestMapping("/v1/Budget")
+public class ControllerBudget {
 
     @GetMapping()
-    public ResponseEntity<String> Test() {
+    public ResponseEntity<String> GetBudget() {
         return ResponseEntity.ok("test");
     }
 }
