731de388eddac937d07e0e328d9d10c6d515e047
Vandijstadt Nicolas <81445233+vandijstadt@users.noreply.github.com>
2025-01-22 13:55:49 +0100

    Modification de labdd

--- christmas_wallet_db_init.sql
+++ christmas_wallet_db_init.sql
@@ -1,11 +1,8 @@
-drop
-database christmas_wallet_db;
+drop database christmas_wallet_db;
 
-create
-database christmas_wallet_db;
+create database christmas_wallet_db;
 
-use
-christmas_wallet_db;
+use christmas_wallet_db;
 
 create table users
 (
@@ -27,7 +24,9 @@ create table budgets
     id     bigint auto_increment primary key,
     budget decimal(10, 2) not null,
     month  bigint         not null,
-    year   bigint         not null
+    year   bigint         not null,
+    userId bigint         not null,
+    constraint FK_userId foreign key (userId) references users (id)
 );
 
 create table purchases
@@ -35,15 +34,13 @@ create table purchases
     id           bigint auto_increment primary key,
     amount       decimal(10, 2) not null,
     purchaseDate datetime       not null,
-    userId       bigint         not null,
     categoryId   bigint         not null,
     budgetId     bigint         not null,
-    constraint FK_userId foreign key (userId) references users (id),
     constraint FK_categoryId foreign key (categoryId) references categories (id),
     constraint FK_budgetId foreign key (budgetId) references budgets (id)
 );
 
-create index idx_purchase_user on purchases (userId);
+create index idx_purchase_user on budgets (userId);
 create index idx_purchase_category on purchases (categoryId);
 create index idx_purchase_date on purchases (purchaseDate);
 
@@ -53,20 +50,19 @@ VALUES ('user1', 'password123'),
        ('user3', 'strongpassword');
 INSERT INTO categories (name)
 VALUES ('Alimentation'),
-       ('Vêtements'),
-       ('Électronique'),
+       ('Vetements'),
+       ('Electronique'),
        ('Cadeaux');
-INSERT INTO budgets (budget, month, year)
-VALUES (1000.00, 12, 2023),
-       (1200.00, 1, 2024),
-       (1500.00, 2, 2024),
-       (150000.00, 2, 2025);
-INSERT INTO purchases (amount, purchaseDate, userId, categoryId, budgetId)
-VALUES (50.00, '2023-12-15 10:30:00', 1, 1, 1),
-       (150.00, '2023-12-20 14:45:00', 2, 2, 1),
-       (200.00, '2024-01-05 09:15:00', 3, 3, 2);
-
-
+INSERT INTO budgets (budget, month, year, userId)
+VALUES (1000.00, 12, 2023, 1),
+       (1200.00, 1, 2024, 1),
+       (1500.00, 2, 2024, 2),
+       (150000.00, 2, 2025, 3);
+INSERT INTO purchases (amount, purchaseDate, categoryId, budgetId)
+VALUES (50.00, '2023-12-15 10:30:00', 1, 1),
+       (150.00, '2023-12-20 14:45:00', 2, 1),
+       (200.00, '2024-01-05 09:15:00', 3, 2);
 
 select *
-from budgets;
+from budgets
+         inner join purchases p on budgets.id = p.budgetId;
